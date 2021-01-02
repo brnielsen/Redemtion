97f9917443e4a981b9e1d99807ebeac23b1c9b3c
brnielsen <47045697+brnielsen@users.noreply.github.com>
2021-01-02 11:45:21 -0500

    Can highlight selectable objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Class for picking up and yeeting objects
+
+    public Camera camera;
+
+    public GameObject selectedObject;
+
+    public bool isHolding = false;
+
+    private float mousePosX, mousePosY;
+
+    public Material highlightedMaterial;
+
+    private Transform _selection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(_selection != null)
+        {
+            var _selectionRenderer = _selection.GetComponent<Renderer>();
+            _selectionRenderer.material = _selection.GetComponent<SelectableObject>().defaultMaterial;
+            _selection = null;
+        }
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            var selectionRenderer = selection.GetComponent<Renderer>();            
+            if (selectionRenderer != null && selection.CompareTag("selectable"))
+            {
+                selectionRenderer.material = highlightedMaterial;
+                _selection = selection;
+            }
+
+           
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            //pickup
+
+        }
+    }
+}
