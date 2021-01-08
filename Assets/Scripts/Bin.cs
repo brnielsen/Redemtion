d774ac19cf8d8f21261a20e0f1f3b26892a64120
brnielsen <47045697+brnielsen@users.noreply.github.com>
2021-01-07 19:46:01 -0500

    Organized prefabs, got basic scoring system working

--- Assets/Scripts/Bin.cs
+++ Assets/Scripts/Bin.cs
@@ -7,16 +7,27 @@ public class Bin : MonoBehaviour
 
     public ReturnableType returnableTypeAccepted;
 
+    private Score score;
+
+    private void Awake()
+    {
+        score = FindObjectOfType<Score>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         SelectableObject selectableObject = other.GetComponent<SelectableObject>();
         if(selectableObject.returnableType == returnableTypeAccepted)
         {
             Debug.Log("Yay points!");
+            score.currentScore += 1;
+            //Play afirmative sound
         }
         else
         {
             Debug.Log("Don't put that in my mouth!");
+            //Play negative sound
+
         }
 
     }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int currentScore;
+    public int finalScore;
+
+    private int potentialScore;
+
+    public bool levelEnd = false;
+
+    private void Awake()
+    {
+        potentialScore = FindObjectsOfType<SelectableObject>().Length;
+        currentScore = 0;
+        finalScore = 0;
+    }
+
+    private void Update()
+    {
+        if(levelEnd == true)
+        {
+            FinalScore();
+        }
+    }
+
+    private int FinalScore()
+    {
+       finalScore = currentScore / potentialScore;
+        return finalScore;
+    }
+}
