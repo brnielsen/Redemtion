39367875b97cade39d5592726fc08d4841f335a8
brnielsen <47045697+brnielsen@users.noreply.github.com>
2021-01-07 19:33:03 -0500

    Added the ability for bins to discern the correct type of returnable that gets thrown in

---
+++ Assets/Scripts/Bin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bin : MonoBehaviour
+{
+
+    public ReturnableType returnableTypeAccepted;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SelectableObject selectableObject = other.GetComponent<SelectableObject>();
+        if(selectableObject.returnableType == returnableTypeAccepted)
+        {
+            Debug.Log("Yay points!");
+        }
+        else
+        {
+            Debug.Log("Don't put that in my mouth!");
+        }
+
+    }
+}
--- Assets/Scripts/SelectableObject.cs
+++ Assets/Scripts/SelectableObject.cs
@@ -6,8 +6,14 @@ public class SelectableObject : MonoBehaviour
 {
     public Material defaultMaterial;
 
+    public ReturnableType returnableType;
+
+
     private void Awake()
     {
         defaultMaterial = GetComponent<Renderer>().material;
     }
+
 }
+
+public enum ReturnableType { medCan, smallCan, beerBottle, wineBottle, plasticBottle, plasticLiter }
