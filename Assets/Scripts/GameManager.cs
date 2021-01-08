30022e87aa70f600329c8fcbfd8a79aeeff8882f
brnielsen <47045697+brnielsen@users.noreply.github.com>
2021-01-08 12:44:46 -0500

    Objects spawn from predetermined list

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    ObjectSpawner objectSpawner;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        objectSpawner = FindObjectOfType<ObjectSpawner>();
+
+        objectSpawner.SpawnObjects();
+    }
+
+
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectListPrefab;
+
+    public Transform spawnLocation;
+    public void SpawnObjects()
+    {
+        List<GameObject> objectList = objectListPrefab.GetComponent<ObjectListHolder>().objectList;
+
+        for (int i = 0; i < objectList.Count; i++)
+        {
+            Instantiate(objectList[i].gameObject, spawnLocation.position, Quaternion.identity);
+
+        }
+       
+    }
+}
